667e8d5d1652a09a89b32654a6bc7fbd4ef31eb0
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2026-05-04 23:26:24 +0400

    feat(add): pawn promotion

--- ChessMain.py
+++ ChessMain.py
@@ -1,7 +1,6 @@
 import numpy
 import pygame as p
-from Chess import ChessEngine
-
+import ChessEngine
 WIDTH = HEIGHT = 800
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
@@ -24,6 +23,9 @@ def main():
     valid_moves = gs.get_valid_moves()
     move_made = False
 
+    promotion_mode = False
+    promotion_square = None
+
     load_images()
     running = True
     square_selected = ()
@@ -34,6 +36,9 @@ def main():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
+                if promotion_mode:
+                    promotion_mode, promotion_square, move_made = handle_promotion_click(e, gs, promotion_square)
+                    continue
                 location = p.mouse.get_pos()
                 col = location[0]//SQ_SIZE
                 row = location[1]//SQ_SIZE
@@ -47,19 +52,30 @@ def main():
                     move = ChessEngine.Move(player_clicks[0], player_clicks[1], gs.board)
                     print(move.getChessNotation())
                     if move in valid_moves:
-                        gs.make_move(move)
-                        move_made = True
+
+                        if (move.piece_moved == 'wp' and move.end_row ==0) or \
+                                (move.piece_moved == 'bp' and move.end_row == 7):
+                            promotion_mode = True
+                            promotion_square = move
+                        else:
+                            gs.make_move(move)
+                            move_made = True
                         square_selected = ()
                         player_clicks = []
                     else:
                         player_clicks = [square_selected]
             elif e.type == p.KEYDOWN:
+                if promotion_mode:
+                    continue
                 if e.key == p.K_z: #undo when z is pressed
                     gs.undo_move()
         if move_made:
             valid_moves = gs.get_valid_moves()
             move_made = False
         drawGameState(screen, gs)
+
+        if promotion_mode:
+            draw_promotion_menu(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -74,6 +90,47 @@ def drawBoard(screen):
         for c in range(DIMENSION):
             color = colors[((r+c) %2)]
             p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def draw_promotion_menu(screen, gs):
+    pieces = ['Q', 'R', 'B', 'N']
+    color = 'w' if gs.whiteToMove else 'b'
+
+    draw_blur(screen)
+
+    menu_width = 4 * SQ_SIZE
+    start_x = (WIDTH - menu_width) // 2
+    y = SQ_SIZE // 2 #Top area
+
+    for i, piece in enumerate(pieces):
+        rect = p.Rect(start_x + i*SQ_SIZE, y, SQ_SIZE, SQ_SIZE)
+        screen.blit(IMAGES[color + piece], rect)
+
+def draw_blur(screen):
+    small = p.transform.smoothscale(screen, (WIDTH//6, HEIGHT//6))
+    blurred = p.transform.smoothscale(small, (WIDTH, HEIGHT))
+    screen.blit(blurred, (0,0))
+
+def handle_promotion_click(event, gs, promotion_square):
+    if event.type == p.MOUSEBUTTONDOWN:
+        x, y = p.mouse.get_pos()
+        col = x // SQ_SIZE
+
+        pieces = ['Q', 'R', 'B', 'N']
+        if 0 <= col < 4:
+            chosen_piece = pieces[col]
+
+            move = promotion_square
+
+            gs.board[move.start_row][move.start_column] = "--"
+            gs.board[move.end_row][move.end_column] = move.piece_moved[0] + chosen_piece
+
+            gs.move_log.append(move)
+            gs.whiteToMove = not gs.whiteToMove
+
+            return False, None, True
+        else:
+            return True, promotion_square, False
+    return True, promotion_square, False
 
 def drawPieces(screen, board):
     for r in range(DIMENSION):
