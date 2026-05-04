bb4eb041ab88dbf1b8a4844f5913365a828e65d3
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2026-05-04 12:12:03 +0400

    feat(add): added rook, knight and bishop moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -11,6 +11,9 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ])
+        self.move_functions = {'p': self.get_pawn_moves, 'R': self.get_rook_moves, 'N': self.get_knight_moves,
+                               'B': self.get_bishop_moves, 'Q': self.get_queen_moves, 'K': self.get_king_moves}
+
         self.whiteToMove = True
         self.move_log = []
     def make_move(self, move):
@@ -37,18 +40,85 @@ class GameState():
                 turn = self.board[row][column][0]
                 if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[row][column][1]
-                    if piece == 'p':
-                        self.get_pawn_moves(row, column, moves)
-                    elif piece == 'R':
-                        self.get_rook_moves(row, column, moves)
+                    self.move_functions[piece](row, column, moves)
 
         return moves
 
 
     def get_pawn_moves(self, row, column, moves):
-        pass
+        if self.whiteToMove:
+            if self.board[row-1][column] == "--": #move forward
+                moves.append(Move((row, column), (row-1, column), self.board))
+                if row == 6 and self.board[row-2][column] == "--":
+                    moves.append(Move((row, column), (row-2, column), self.board))
+            if column - 1 >= 0:
+                if self.board[row-1][column-1][0] == 'b': #enemy to capture in diagonal
+                    moves.append(Move((row, column), (row-1, column-1), self.board))
+            if column + 1 <len(self.board): #captures to the right
+                if self.board[row-1][column+1][0] == 'b':
+                    moves.append(Move((row, column), (row-1, column+1), self.board))
+        else:
+            if self.board[row+1][column] == "--": #Move forward as black
+                moves.append(Move((row, column), (row+1, column), self.board))
+                if row == 1 and self.board[row+2][column] == "--":
+                    moves.append(Move((row, column), (row+2, column), self.board))
+            if column - 1>=0: #Capture left
+                if self.board[row+1][column-1][0] == 'w':
+                    moves.append(Move((row, column), (row+1, column-1), self.board))
+            if column + 1<len(self.board): #Capture right
+                if self.board[row+1][column+1][0] == 'w':
+                    moves.append(Move((row, column), (row+1, column+1), self.board))
 
     def get_rook_moves(self, row, column, moves):
+        directions = ((-1,0), (0, -1), (1, 0), (0, 1)) #up, left, down, right movements
+        enemy_color = 'b' if self.whiteToMove else 'w'
+        for d in directions:
+            for i in range(1, 8):
+                end_row = row + d[0]*i
+                end_column = column + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_column < 8:
+                    end_piece = self.board[end_row][end_column]
+                    if end_piece == "--":
+                        moves.append(Move((row, column), (end_row, end_column), self.board))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((row, column), (end_row, end_column), self.board))
+                        break
+                    else: #Cant capture own piece
+                        break
+                else: #off board
+                    break
+
+    def get_knight_moves(self, row, column, moves):
+        knight_moves = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1,2), (2, -1), (2, 1)) #All L-s
+        ally_color = 'w' if self.whiteToMove else 'b'
+        for m in knight_moves:
+            end_row = row + m[0]
+            end_column = column + m[1]
+            if 0 <= end_row < 8 and 0 <= end_column < 8:
+                end_piece = self.board[end_row][end_column]
+                if end_piece[0] != ally_color: #Opposite color -> legit move to capture it
+                    moves.append(Move((row, column), (end_row, end_column), self.board))
+    def get_bishop_moves(self, row, column, moves):
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemy_color = 'b' if self.whiteToMove else 'w'
+        for d in directions:
+            for i in range(1, 8):
+                end_row = row + d[0] * i
+                end_column = column + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_column < 8:
+                    end_piece = self.board[end_row][end_column]
+                    if end_piece == "--":
+                        moves.append(Move((row, column), (end_row, end_column), self.board))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((row, column), (end_row, end_column), self.board))
+                        break
+                    else: #Cant capture own piece
+                        break
+                else:
+                    break
+    def get_queen_moves(self, row, column, moves):
+        pass
+    def get_king_moves(self, row, column, moves):
         pass
 
 class Move():
